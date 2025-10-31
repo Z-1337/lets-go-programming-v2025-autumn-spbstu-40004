250ecf0fecb4d254b8586eb237fab602fe6726fc
Danil <un228der@gmail.com>
2025-10-31 21:28:46 +0300

    [TASK-3] Fixes

--- kushekbaev.danil/task-3/cmd/service/main.go
+++ kushekbaev.danil/task-3/cmd/service/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"github.com/KiRy6A/task-3/internal/configreader"
-	"github.com/KiRy6A/task-3/internal/data"
-	"github.com/KiRy6A/task-3/internal/valutemanager"
+	"github.com/Z-1337/task-3/internal/cfgreader"
+	"github.com/Z-1337/task-3/internal/currencyparser"
+	"github.com/Z-1337/task-3/internal/currencymanager"
 )
 
 func main() {
-	config, err := configreader.Parse()
+	cfg, err := cfgreader.Parse()
 	if err != nil {
 		panic("Config parsing error: " + err.Error())
 	}
 
-	valutes, err := valutemanager.Read(config.Input)
+	currencies, err := currencymanager.Read(cfg.Input)
 	if err != nil {
 		panic("Reading error: " + err.Error())
 	}
 
-	valutes.AllValutes = data.Sort(valutes.AllValutes)
+	currencies.Allcurrencies = data.Sort(currencies.AllCurrencies)
 
-	err = valutemanager.Write(config.Output, valutes)
+	err = currencymanager.Write(cfg.Output, currencies)
 	if err != nil {
 		panic("Writing error: " + err.Error())
 	}
